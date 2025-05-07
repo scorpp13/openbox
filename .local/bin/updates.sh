9259bfa5fdd2d1f1ec420893d04a8be6a5619cfd
as <scorpp13@gmail.com>
2025-05-07 22:04:47 +0200

    upd

--- .local/bin/updates.sh
+++ .local/bin/updates.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/bash
 threshhold_zero=0
 icon=$HOME/.local/bin/suse_logo.svg
-updates=$(zypper lu -a | grep v | wc -l)
+updates=$(zypper lu | grep v | wc -l)
 if [ "$updates" -gt $threshhold_zero ]; then
     echo "$icon"
     dunstify --raw_icon=$HOME/.local/bin/suse_logo.svg "Updates available" "<b>$updates</b>"
