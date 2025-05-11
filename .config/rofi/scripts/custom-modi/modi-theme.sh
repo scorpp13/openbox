2c0560356942d2f444c3358733e0f7ade8372edf
as <scorpp13@gmail.com>
2025-05-11 14:20:11 +0200

    update

--- .config/rofi/scripts/custom-modi/modi-theme.sh
+++ .config/rofi/scripts/custom-modi/modi-theme.sh
@@ -17,8 +17,7 @@ C_='' C="<span font_desc='${ROW_ICON_FONT}' weight='bold'>${C_}</span>   Duns
 D_='' D="<span font_desc='${ROW_ICON_FONT}' weight='bold'>${D_}</span>   Openbox Theme"
 E_='' E="<span font_desc='${ROW_ICON_FONT}' weight='bold'>${E_}</span>   Tint2 Theme"
 F_='' F="<span font_desc='${ROW_ICON_FONT}' weight='bold'>${F_}</span>   GTK Theme"
-G_='' G="<span font_desc='${ROW_ICON_FONT}' weight='bold'>${G_}</span>   Qt5 Theme"
-H_='' H="<span font_desc='${ROW_ICON_FONT}' weight='bold'>${H_}</span>   Qt6 Theme"
+G_='' G="<span font_desc='${ROW_ICON_FONT}' weight='bold'>${G_}</span>   Qt Theme"
 
 case "${@}" in
     "$A") eval 'exec waypaper >&2'
@@ -33,15 +32,13 @@ case "${@}" in
     ;;
     "$F") eval 'exec lxappearance >&2'
     ;;
-    "$G") eval 'exec qt5ct >&2'
-    ;;
-    "$H") eval 'exec qt6ct >&2'
+    "$G") eval 'exec qt6ct >&2'
     ;;
 esac
 
 MESSAGE="<span font_desc='${MSG_ICON_FONT}' weight='bold'></span>"
 
 printf '%b\n' '\0use-hot-keys\037true' '\0markup-rows\037true' "\0message\037${MESSAGE}" \
-              "$A" "$B" "$C" "$D" "$E" "$F" "$G" "$H"
+              "$A" "$B" "$C" "$D" "$E" "$F" "$G"
 
 exit ${?}
